195a3fec05624eaa8f59b958213d165eed8392a6
doppelmarker <doppelmarker@gmail.com>
2020-10-01 13:18:44 +0300

    feat: add solution to typical-arrays-problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+  if (array === undefined || array.length === 0) return 0;
+  let length = array.length, min = Infinity;
+  while (length--) {
+    if (array[length] < min) {
+      min = array[length]
+    }
+  }
+  return min;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+  if (array === undefined || array.length === 0) return 0;
+  let length = array.length, max = -Infinity;
+  while (length--) {
+    if (array[length] > max) {
+      max = array[length]
+    }
+  }
+  return max;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+  if (array === undefined || array.length === 0) return 0;
+
+  return array.reduce((a, item) => {
+    return a + item;
+  }, 0) / array.length;
 }
